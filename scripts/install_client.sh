44029c0ee5f58ac27f83dd268eb69dfd3a0d0889
Mcgill <erinmc@f45c89b0babb.ant.amazon.com>
2017-03-14 18:20:15 -0400

    update with startup script

---
+++ scripts/install_client.sh
@@ -0,0 +1,88 @@
+#!/usr/bin/env bash
+set -ex
+
+# This script will automatically install nomad, put it in /usr/local/bin, and create /etc/nomad.d directory
+HOSTNAME=`hostname`
+LOCAL_IP=`curl http://169.254.169.254/latest/meta-data/local-ipv4`
+DATACENTER=`curl http://169.254.169.254/latest/dynamic/instance-identity/document | jq .region -r`
+curl -sSL https://releases.hashicorp.com/nomad/0.5.4/nomad_0.5.4_linux_amd64.zip -o /tmp/nomad.zip
+HASHI_SOURCE_SHA256SUM=`curl https://releases.hashicorp.com/nomad/0.5.4/nomad_0.5.4_SHA256SUMS?_ga=1.245768324.1608334354.1478014704 | grep nomad_0.5.4_linux_amd64.zip | awk -F' ' '{print $1}'`
+DOWNLOAD_SHA256SUM=`sha256sum /tmp/nomad.zip | awk -F' ' '{print $1}'`
+
+if [[ "${DOWNLOAD_SHA256SUM}" != "${HASHI_SOURCE_SHA256SUM}" ]]
+then
+  echo "The SHA256 for your download does not match the source-provided checksum. Exiting."
+  exit 1
+fi
+unzip -d /tmp /tmp/nomad.zip
+chmod 755 /tmp/nomad
+mv /tmp/nomad /usr/local/bin/
+if [ ! -d /opt/nomad ]
+then
+  mkdir -p /opt/nomad/config && mkdir /opt/nomad/data
+fi
+
+#######################
+# Nomad Client Config #
+#######################
+touch /opt/nomad/config/client.hcl
+cat > /opt/nomad/config/client.hcl <<EOF
+bind_addr = "0.0.0.0"
+
+log_level = "DEBUG"
+
+data_dir = "/opt/nomad"
+
+name = "$HOSTNAME"
+
+datacenter = "$DATACENTER"
+
+client {
+  enabled = true
+}
+
+
+addresses {
+  rpc  = "$LOCAL_IP"
+  serf = "$LOCAL_IP"
+}
+advertise {
+  http = "$LOCAL_IP:4646"
+}
+
+consul {
+  # The address to the Consul agent.
+  address = "127.0.0.1:8500"
+
+  # The service name to register the server and client with Consul.
+  client_service_name = "nomad-client"
+
+  # Enables automatically registering the services.
+  auto_advertise = true
+
+  # Enabling the server and client to bootstrap using Consul.
+  client_auto_join = true
+}
+EOF
+
+sudo tee /etc/init/nomad.conf > /dev/null <<EOF
+description "Nomad"
+start on runlevel [2345]
+stop on runlevel [!2345]
+respawn
+console log
+script
+  if [ -f "/etc/service/nomad" ]; then
+    . /etc/service/nomad
+  fi
+  exec /usr/local/bin/nomad agent \
+    -config="/opt/nomad/config/client.hcl" \
+    >>/var/log/nomad.log 2>&1
+end script
+EOF
+
+#######################################
+# START SERVICES
+#######################################
+
+sudo service nomad start
---
+++ scripts/install_server.sh
@@ -0,0 +1,82 @@
+#!/usr/bin/env bash
+set -ex
+
+# This script will automatically install nomad, put it in /usr/local/bin, and create /etc/nomad.d directory
+HOSTNAME=`hostname`
+LOCAL_IP=`curl http://169.254.169.254/latest/meta-data/local-ipv4`
+curl -sSL https://releases.hashicorp.com/nomad/0.5.4/nomad_0.5.4_linux_amd64.zip -o /tmp/nomad.zip
+HASHI_SOURCE_SHA256SUM=`curl https://releases.hashicorp.com/nomad/0.5.4/nomad_0.5.4_SHA256SUMS?_ga=1.245768324.1608334354.1478014704 | grep nomad_0.5.4_linux_amd64.zip | awk -F' ' '{print $1}'`
+DOWNLOAD_SHA256SUM=`sha256sum /tmp/nomad.zip | awk -F' ' '{print $1}'`
+
+if [[ "${DOWNLOAD_SHA256SUM}" != "${HASHI_SOURCE_SHA256SUM}" ]]
+then
+  echo "The SHA256 for your download does not match the source-provided checksum. Exiting."
+  exit 1
+fi
+unzip -d /tmp /tmp/nomad.zip
+chmod 755 /tmp/nomad
+mv /tmp/nomad /usr/local/bin/
+if [ ! -d /opt/nomad ]
+then
+  mkdir -p /opt/nomad/config && mkdir /opt/nomad/data
+fi
+
+#######################
+# Nomad Server Config #
+#######################
+
+touch /opt/nomad/config/server.hcl
+cat > /opt/nomad/config/server.hcl <<EOF
+bind_addr = "0.0.0.0"
+
+log_level = "DEBUG"
+
+data_dir = "/opt/nomad"
+
+name = "$HOSTNAME"
+
+server {
+  enabled = true
+  bootstrap_expect = 3
+}
+advertise {
+  http = "$LOCAL_IP"
+  rpc = "$LOCAL_IP"
+  serf = "$LOCAL_IP"
+}
+consul {
+  # The address to the Consul agent.
+  address = "127.0.0.1:8500"
+
+  # The service name to register the server with Consul.
+  server_service_name = "nomad"
+
+  # Enables automatically registering the services.
+  auto_advertise = true
+
+  # Enabling the server to bootstrap using Consul.
+  server_auto_join = true
+}
+EOF
+
+tee /etc/init/nomad.conf > /dev/null <<EOF
+description "Nomad"
+start on runlevel [2345]
+stop on runlevel [!2345]
+respawn
+console log
+script
+  if [ -f "/etc/service/nomad" ]; then
+    . /etc/service/nomad
+  fi
+  exec /usr/local/bin/nomad agent \
+    -config="/opt/nomad/config/server.hcl" \
+    >>/var/log/nomad.log 2>&1
+end script
+EOF
+
+#######################################
+# START SERVICES
+#######################################
+
+service nomad start
